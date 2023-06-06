a23a0e59a5a96cc190aa5b07809abfb63aef8249
azelky <naamcham@gmail.com>
2023-06-07 00:16:05 +0200

    feat: ✨ initial commit - theme creation

---
+++ README.md
@@ -0,0 +1,42 @@
+# README
+
+## Hello 👋
+
+This is my first theme 🤟  
+Yes, I like bright colors 💜
+
+## Neki
+
+Theme created for a HTML/CSS/JS (Angular) environment 💻  
+Special thanks to Sarah Drasner for [the great article](https://css-tricks.com/creating-a-vs-code-theme/) ✨
+
+Inspired by [Material Palenight Theme](https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme) and [Panda Theme](https://marketplace.visualstudio.com/items?itemName=tinkertrain.theme-panda).
+
+## How to activate the theme ✨
+
+Go to File > Preferences > Color Themes and find **Neki**.
+
+## Matching colors for terminal 🌸
+
+    "workbench.colorCustomizations" : {
+      "terminal.background":"#28273A",
+      "terminal.foreground":"#E9E9F4",
+      "terminal.ansiBlack":"#282936",
+      "terminalCursor.background": "#E6E6E6",
+      "terminalCursor.foreground": "#FF4B82",
+      "terminal.ansiBlue": "#45A9F9",
+      "terminal.ansiBrightBlack": "#757575",
+      "terminal.ansiBrightBlue": "#6FC1FF",
+      "terminal.ansiBrightCyan": "#BCAAFE",
+      "terminal.ansiBrightGreen": "#19f9d8",
+      "terminal.ansiBrightMagenta": "#FF9AC1",
+      "terminal.ansiBrightRed": "#FF2C6D",
+      "terminal.ansiBrightWhite": "#E6E6E6",
+      "terminal.ansiBrightYellow": "#FFCC95",
+      "terminal.ansiCyan": "#B084EB",
+      "terminal.ansiGreen": "#19f9d8",
+      "terminal.ansiMagenta": "#ff82bc",
+      "terminal.ansiRed": "#ff5797",
+      "terminal.ansiWhite": "#CDCDCD",
+      "terminal.ansiYellow": "#FFC07D"
+    }
---
+++ changelog.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  disableEmoji: false,
+  list: [
+    "feat",
+    "fix",
+    "style",
+    "refactor",
+    "chore",
+    "docs",
+    "test",
+    "perf",
+    "ci",
+  ],
+  maxMessageLength: 80,
+  minMessageLength: 3,
+  questions: [
+    "type",
+    "scope",
+    "subject",
+    "body",
+    "breaking",
+    "issues",
+    "lerna",
+  ],
+  scopes: [],
+  types: {
+    chore: {
+      description: "Build process or auxiliary tool changes",
+      emoji: "🌱",
+      value: "chore",
+    },
+    ci: {
+      description: "CI related changes",
+      emoji: "🎡",
+      value: "ci",
+    },
+    docs: {
+      description: "Documentation only changes",
+      emoji: "👻",
+      value: "docs",
+    },
+    feat: {
+      description: "A new feature",
+      emoji: "✨",
+      value: "feat",
+    },
+    fix: {
+      description: "A bug fix",
+      emoji: "🐛",
+      value: "fix",
+    },
+    perf: {
+      description: "A code change that improves performance",
+      emoji: "⚡️",
+      value: "perf",
+    },
+    refactor: {
+      description: "A code change that neither fixes a bug or adds a feature",
+      emoji: "🌻",
+      value: "refactor",
+    },
+    release: {
+      description: "Create a release commit",
+      emoji: "🌞",
+      value: "release",
+    },
+    style: {
+      description: "Markup, white-space, formatting, missing semi-colons...",
+      emoji: "🌸",
+      value: "style",
+    },
+    test: {
+      description: "Adding missing tests",
+      emoji: "💫",
+      value: "test",
+    },
+  },
+};
